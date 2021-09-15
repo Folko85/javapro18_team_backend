652e0088f92210672a976881cd78af6bff758fcc
Shkiller <nicklyashko9@gmail.com>
2021-09-15 14:26:52 +0300

    MailSender

---
+++ src/main/java/com/skillbox/socialnetwork/service/MailSender.java
@@ -0,0 +1,32 @@
+package com.skillbox.socialnetwork.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSender {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Autowired
+    public MailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(String emailTo, String message) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom(username);
+        mailMessage.setTo(emailTo);
+        mailMessage.setText(message);
+
+        mailSender.send(mailMessage);
+    }
+
+}
