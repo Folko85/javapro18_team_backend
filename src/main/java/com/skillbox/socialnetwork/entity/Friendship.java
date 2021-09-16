b79cebafd0f0352f671f3bca95c3fe064f427637
Кот Артем <artem70107@gmail.com>
2021-09-16 18:30:13 +0300

    небольшие изменения

---
+++ src/main/java/com/skillbox/socialnetwork/entity/Friendship.java
@@ -0,0 +1,43 @@
+package com.skillbox.socialnetwork.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table (name = "Friendship")
+@Getter
+@Setter
+public class Friendship {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @JoinColumn (columnDefinition = "status_id")
+    @OneToOne
+    private FriendshipStatus status;
+
+    @ManyToOne
+    @JoinColumn (name = "src_person_id")
+    private Person srcPerson;
+
+    @ManyToOne
+    @JoinColumn (name = "dst_person_id")
+    private Person dstPerson;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Friendship that = (Friendship) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
