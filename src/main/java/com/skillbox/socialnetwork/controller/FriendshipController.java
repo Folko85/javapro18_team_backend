bfa0fd26e315ce8b1427c813d35f1cd738873266
Кот Артем <artem70107@gmail.com>
2021-09-22 21:15:22 +0300

    Удалил поле name из таблицы Friendship Status. Добавил changeSet.

--- src/main/java/com/skillbox/socialnetwork/controller/FriendshipController.java
+++ src/main/java/com/skillbox/socialnetwork/controller/FriendshipController.java
@@ -1,6 +1,9 @@
 package com.skillbox.socialnetwork.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.skillbox.socialnetwork.api.response.AuthDTO.Place;
 import com.skillbox.socialnetwork.entity.Friendship;
+import com.skillbox.socialnetwork.entity.Person;
 import com.skillbox.socialnetwork.repository.FriendshipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,19 +20,32 @@ public class FriendshipController {
     private FriendshipRepository friendshipRepository;
 
     @GetMapping("/api/v1/friends")
-    public List<Friendship> list() {
+    public ResponseEntity<?> list() {
+
         Iterable<Friendship> taskIterable = friendshipRepository.findAll();
+
         ArrayList<Friendship> friendsList = new ArrayList<>();
+
         for (Friendship friendship : taskIterable) {
+            Person person = friendship.getDstPerson();
+
             friendsList.add(friendship);
         }
-        return friendsList;
+
+        if (!friendsList.isEmpty()) {
+            return new ResponseEntity<>(friendsList, HttpStatus.OK);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
     }
 
     @DeleteMapping("/api/v1/friends/{id}")
-    public void delete(@PathVariable int id) {
+    public ResponseEntity<?> delete(@PathVariable int id) {
         if (friendshipRepository.existsById(id)) {
             friendshipRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
     }
 
