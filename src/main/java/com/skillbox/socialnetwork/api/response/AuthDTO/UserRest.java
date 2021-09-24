0a63c8d5f132590a824186259c95ef24c20dce0a
GutorovMikhail <Folko85@yandex.ru>
2021-09-25 00:29:48 +0300

    Переделал под себя получение информации о пользователе

--- src/main/java/com/skillbox/socialnetwork/api/response/AuthDTO/UserRest.java
+++ src/main/java/com/skillbox/socialnetwork/api/response/AuthDTO/UserRest.java
@@ -4,31 +4,28 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.time.Instant;
+
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserRest {
-    private int id;
     @JsonProperty("first_name")
     private String firstName;
+    private long timestamp;
     @JsonProperty("last_name")
     private String lastName;
     @JsonProperty("reg_date")
-    private long dateAndTimeOfRegistration;
+    private Instant dateAndTimeOfRegistration;
     @JsonProperty("birth_date")
-    private long birthDate;
-    @JsonProperty("email")
-    private String eMail;
-    private String phone;
-    private String photo;
-    private String about;
-    private Place city;
-    private Place country;
-    @JsonProperty("messages_permission")
-    private String messagesPermission;
+    private Instant birthDate;
     @JsonProperty("last_online_time")
     private long lastOnlineTime;
     @JsonProperty("is_blocked")
     private boolean isBlocked;
-    private String error;
-    private long timestamp;
+    @JsonProperty("phone")
+    private String phone;
+    @JsonProperty("city")
+    private String city;
+    @JsonProperty("country")
+    private String country;
 }
--- src/main/java/com/skillbox/socialnetwork/controller/UserController.java
+++ src/main/java/com/skillbox/socialnetwork/controller/UserController.java
@@ -1,29 +1,32 @@
 package com.skillbox.socialnetwork.controller;
 
 import com.skillbox.socialnetwork.api.response.AuthDTO.UserRest;
-import com.skillbox.socialnetwork.service.UserServiceImpl;
+import com.skillbox.socialnetwork.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
 @RequestMapping("/api/v1/users")
 public class UserController {
-    private  UserServiceImpl userService;
-    public UserController(UserServiceImpl userService){
-        this.userService= userService;
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
 
     }
 
     @GetMapping("/me")
-    public ResponseEntity<UserRest> login() throws Exception {
-            String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
+    public UserRest getInfo() throws Exception {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userService.getUserByEmail(email);
+    }
+
+    @PutMapping("/me")
+    public UserRest changeInfo(@RequestBody UserRest userUpdated) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userService.updateUserByEmail(email, userUpdated);
     }
 
 
