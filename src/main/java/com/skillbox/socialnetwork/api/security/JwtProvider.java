43c82ee6acd7d8948d7415803b79145cdd86b6d8
tkabrat@gmail.com <aa8TEajyASksdgV>
2021-09-26 18:16:58 +0500

    Grafana

--- src/main/java/com/skillbox/socialnetwork/api/security/JwtProvider.java
+++ src/main/java/com/skillbox/socialnetwork/api/security/JwtProvider.java
@@ -1,7 +1,6 @@
 package com.skillbox.socialnetwork.api.security;
 
 import io.jsonwebtoken.*;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
