be90db52343d045085686813931752ee5516959b
Shkiller <nicklyashko9@gmail.com>
2021-09-27 16:47:57 +0300

    костыль на даты

--- src/main/java/com/skillbox/socialnetwork/controller/PostController.java
+++ src/main/java/com/skillbox/socialnetwork/controller/PostController.java
@@ -22,8 +22,8 @@ public class PostController {
 
     @GetMapping("/post")
     public ResponseEntity<PostResponse> getPosts(@RequestParam(name = "text", defaultValue = "") String text,
-                                                 @RequestParam(name = "date_from") long dateFrom,
-                                                 @RequestParam(name = "date_to") long dateTo,
+                                                 @RequestParam(name = "date_from", defaultValue = "0") long dateFrom,
+                                                 @RequestParam(name = "date_to", defaultValue = "1701214256861") long dateTo,
                                                  @RequestParam(name = "offset", defaultValue = "0") int offset,
                                                  @RequestParam(name = "itemPerPage", defaultValue = "20") int itemPerPage) {
         return new ResponseEntity<>(postService.getPosts(text,dateFrom,dateTo,offset,itemPerPage),HttpStatus.OK);
