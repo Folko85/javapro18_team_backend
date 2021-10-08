7c60cfdea8ed941562ba4183f4302fbd649e6aae
Кот Артем <artem70107@gmail.com>
2021-10-08 20:09:31 +0300

    Внес небольшие изменения

--- src/main/java/com/skillbox/socialnetwork/api/request/SearchUser.java
+++ src/main/java/com/skillbox/socialnetwork/api/request/SearchUser.java
@@ -1,7 +1,8 @@
 package com.skillbox.socialnetwork.api.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SearchUser {
 
     @JsonProperty("first_name")
