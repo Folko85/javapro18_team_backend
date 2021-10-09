4e25f86c8503069fd1e020bd234db4f025092e83
Ivan Zu2kel <ivan_zukkel@mail.ru>
2021-10-09 20:37:10 +0300

    Окончательный вариант, дубль 2

--- src/main/java/com/skillbox/socialnetwork/api/response/PostDTO/PostResponse.java
+++ src/main/java/com/skillbox/socialnetwork/api/response/PostDTO/PostResponse.java
@@ -1,4 +1,4 @@
-package com.skillbox.socialnetwork.api.response.PostDTO;
+package com.skillbox.socialnetwork.api.response.postdto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
@@ -16,13 +16,4 @@ public class PostResponse {
     private int offset;
     private int perPage;
     private List<?> data;
-
-    public PostResponse(int total, int offset, int perPage, List<?> data) {
-        this.error = "string";
-        timestamp = System.currentTimeMillis();
-        this.total = total;
-        this.offset = offset;
-        this.perPage = perPage;
-        this.data = data;
-    }
 }
