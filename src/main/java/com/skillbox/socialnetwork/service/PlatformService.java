f8bb99533eb6e5953548800967a733bd35807ebe
Brosha <smileanton@yandex.ru>
2021-10-12 03:29:42 +0300

    JP18-50 заглушка для языков.

--- src/main/java/com/skillbox/socialnetwork/service/PlatformService.java
+++ src/main/java/com/skillbox/socialnetwork/service/PlatformService.java
@@ -1,6 +1,9 @@
 package com.skillbox.socialnetwork.service;
 
 import com.skillbox.socialnetwork.api.response.CityDTO;
+import com.skillbox.socialnetwork.api.response.Dto;
+import com.skillbox.socialnetwork.api.response.ListResponse;
+import com.skillbox.socialnetwork.api.response.platformdto.Language;
 import com.vk.api.sdk.client.Lang;
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
@@ -11,11 +14,15 @@ import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.base.Country;
 import com.vk.api.sdk.objects.database.responses.GetCitiesResponse;
 import com.vk.api.sdk.objects.database.responses.GetCountriesResponse;
+import liquibase.pro.packaged.L;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -51,4 +58,23 @@ public class PlatformService {
             return result;
         }).collect(Collectors.toList());
     }
+    /*
+        TODO: Это заглушка
+    */
+
+    public ListResponse getLanguages() {
+        ListResponse listResponse = new ListResponse();
+        listResponse.setTimestamp(Instant.now());
+        listResponse.setTotal(1);
+        listResponse.setOffset(0);
+        listResponse.setPerPage(1);
+        Language language = new Language();
+        language.setId(1);
+        language.setTitle("Русский");
+        List<Dto> languages= new ArrayList<>();
+        languages.add(language);
+        listResponse.setData(languages);
+        return listResponse;
+    }
+
 }
