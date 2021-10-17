921873a36b75a33679d3ee433e49cea02381f334
GutorovMikhail <folko85@yandex.ru>
2021-10-17 20:27:44 +0300

    Фиксы зависимостей. Пытаюсь завести прометея локально.

--- src/main/java/com/skillbox/socialnetwork/controller/StorageController.java
+++ src/main/java/com/skillbox/socialnetwork/controller/StorageController.java
@@ -2,6 +2,7 @@ package com.skillbox.socialnetwork.controller;
 
 import com.skillbox.socialnetwork.api.response.DataResponse;
 import com.skillbox.socialnetwork.service.StorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +13,7 @@ import java.io.IOException;
 import java.security.Principal;
 
 @RestController
+@Tag(name = "Контроллер для загрузки файлов")
 public class StorageController {
 
     private final StorageService storageService;
