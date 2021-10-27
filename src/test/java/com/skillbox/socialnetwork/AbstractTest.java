bd1e59bcb3e0ccd48b393eec464980e40e01163e
Gutorov <Folko85@yandex.ru>
2021-10-27 11:39:25 +0300

    Фикс сваггера

--- src/test/java/com/skillbox/socialnetwork/AbstractTest.java
+++ src/test/java/com/skillbox/socialnetwork/AbstractTest.java
@@ -1,9 +1,11 @@
 package com.skillbox.socialnetwork;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import liquibase.Liquibase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
