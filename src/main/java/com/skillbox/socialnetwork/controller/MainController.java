b2d45bf53d504787a7e54f8ff1998066cb3e60f3
Ivan Fomkin <ivan-fomkin@yandex.ru>
2021-10-30 00:40:00 +0300

    Добавлен Main Controller Исправлена ошибка 404 при обновлении страницы по любому URL, кроме корневого

---
+++ src/main/java/com/skillbox/socialnetwork/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.skillbox.socialnetwork.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String getFrontend() {
+        return "index";
+    }
+
+    @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET}, value = "/**/{path:[^\\.]*}")
+    public String all() {
+        return "forward:/";
+    }
+}
