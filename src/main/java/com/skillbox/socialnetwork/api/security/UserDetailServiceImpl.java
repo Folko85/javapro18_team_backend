106a996f48267e28d872a4f1bb4ab0b1597322d4
Gutorov <Folko85@yandex.ru>
2021-10-31 21:30:12 +0300

    Теперь UserDetailServiceImpl используется только в фильтре. Логику дополнительных проверок перенёс в AuthService

--- src/main/java/com/skillbox/socialnetwork/api/security/UserDetailServiceImpl.java
+++ src/main/java/com/skillbox/socialnetwork/api/security/UserDetailServiceImpl.java
@@ -1,29 +1,20 @@
 package com.skillbox.socialnetwork.api.security;
 
-import com.skillbox.socialnetwork.entity.Person;
 import com.skillbox.socialnetwork.repository.PersonRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
     private final PersonRepository personRepository;
 
     public UserDetailServiceImpl(PersonRepository personRepository) {
-            this.personRepository = personRepository;
-        }
-
-        @Override
-        public UserDetails loadUserByUsername(String eMail) {
-            Optional<Person> optionalPerson = personRepository.findByEMail(eMail);
-            if (optionalPerson.isPresent() && optionalPerson.get().isApproved() && !optionalPerson.get().isDeleted()) {
-                Person person = optionalPerson.get();
-                person.setLastOnlineTime(LocalDateTime.now());
-                return personRepository.save(person);
-            } else return null;
-        }
+        this.personRepository = personRepository;
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String eMail) {
+        return personRepository.findByEMail(eMail).orElse(null);
+    }
+}
