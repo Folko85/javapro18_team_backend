47e616b87242234a476e5d22afe623f9a3829abb
Gutorov <Folko85@yandex.ru>
2021-11-03 22:20:47 +0300

    Может так

--- src/test/java/com/skillbox/socialnetwork/controller/CommentControllerTest.java
+++ src/test/java/com/skillbox/socialnetwork/controller/CommentControllerTest.java
@@ -14,21 +14,24 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = {NetworkApplication.class})
-@DirtiesContext
+@AutoConfigureMockMvc
 class CommentControllerTest extends AbstractTest {
 
     @Autowired
@@ -81,9 +84,9 @@ class CommentControllerTest extends AbstractTest {
 
 
     @AfterEach
-    public void resetDb() {
-        postRepository.deleteAll();
+    public void cleanup() {
         commentRepository.deleteAll();
+        postRepository.deleteAll();
         personRepository.deleteAll();
     }
 
