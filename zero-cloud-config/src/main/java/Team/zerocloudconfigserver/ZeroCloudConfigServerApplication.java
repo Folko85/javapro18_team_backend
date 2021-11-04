2abf4b3b1098c1fb9e83b375c80d9e2b6ae71e2f
tkabrat@gmail.com <aa8TEajyASksdgV>
2021-11-04 19:26:23 +0500

    Новая структура проекта

---
+++ zero-cloud-config/src/main/java/Team/zerocloudconfigserver/ZeroCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package Team.zerocloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ZeroCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroCloudConfigServerApplication.class, args);
+	}
+
+}
