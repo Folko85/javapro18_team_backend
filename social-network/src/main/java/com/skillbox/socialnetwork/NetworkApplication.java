eef8dfd95f9518a5de567aa818a3d87c9916c10c
Кот Артем <artem70107@gmail.com>
2021-11-07 13:14:16 +0300

    поправил pomники

--- social-network/src/main/java/com/skillbox/socialnetwork/NetworkApplication.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/NetworkApplication.java
@@ -1,14 +1,11 @@
 package com.skillbox.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 public class NetworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkApplication.class, args);
+    }
 
 }
