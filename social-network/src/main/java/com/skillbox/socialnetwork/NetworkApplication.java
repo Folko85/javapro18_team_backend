b56f502d2e2b51007880bb7189e13221854c8099
Gutorov <Folko85@yandex.ru>
2021-11-07 18:02:42 +0300

    Вернул аннотации

--- social-network/src/main/java/com/skillbox/socialnetwork/NetworkApplication.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/NetworkApplication.java
@@ -1,7 +1,11 @@
 package com.skillbox.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@SpringBootApplication
 public class NetworkApplication {
 
     public static void main(String[] args) {
