17a66f747e8ca3ae8451f76b707a661c64bd3f69
Gutorov <Folko85@yandex.ru>
2021-11-08 23:43:25 +0300

    Добавил метод удаления фото

--- social-network/src/main/java/com/skillbox/socialnetwork/controller/StorageController.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/controller/StorageController.java
@@ -1,11 +1,14 @@
 package com.skillbox.socialnetwork.controller;
 
+import com.skillbox.socialnetwork.api.response.AccountResponse;
 import com.skillbox.socialnetwork.api.response.DataResponse;
 import com.skillbox.socialnetwork.api.response.platformdto.ImageDto;
 import com.skillbox.socialnetwork.service.StorageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,13 @@ public class StorageController {
     @Operation (summary = "Загрузка изображения")
     public DataResponse<ImageDto> uploadImage(@RequestParam("file") MultipartFile file, @RequestParam String type, Principal principal) throws IOException {
         return storageService.uploadImage(file, type, principal);
+    }
+
+    @DeleteMapping("/api/v1/storage/{id}")
+    @PreAuthorize("hasAuthority('user:write')")
+    @Operation (summary = "Загрузка изображения")
+    public AccountResponse deleteImage(@PathVariable int id, Principal principal) throws IOException {
+        return storageService.deleteImage(id, principal);
     }
 
 }
--- social-network/src/main/java/com/skillbox/socialnetwork/service/StorageService.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/service/StorageService.java
@@ -3,9 +3,12 @@ package com.skillbox.socialnetwork.service;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
+import com.skillbox.socialnetwork.api.response.AccountResponse;
 import com.skillbox.socialnetwork.api.response.DataResponse;
 import com.skillbox.socialnetwork.api.response.platformdto.ImageDto;
 import com.skillbox.socialnetwork.entity.Person;
+import com.skillbox.socialnetwork.entity.PostFile;
+import com.skillbox.socialnetwork.repository.FileRepository;
 import com.skillbox.socialnetwork.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -13,12 +16,14 @@ import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededExceptio
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.IOException;
 import java.security.Principal;
 import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -27,6 +32,8 @@ import java.util.concurrent.ThreadLocalRandom;
 public class StorageService {
 
     private final PersonRepository personRepository;
+
+    private final FileRepository fileRepository;
 
     @Value("${external.cloudinary.cloud}")
     private String cloud;
@@ -37,8 +44,9 @@ public class StorageService {
     @Value("${external.cloudinary.secret}")
     private String secret;
 
-    public StorageService(PersonRepository personRepository) {
+    public StorageService(PersonRepository personRepository, FileRepository fileRepository) {
         this.personRepository = personRepository;
+        this.fileRepository = fileRepository;
     }
 
     public DataResponse<ImageDto> uploadImage(MultipartFile image, String type, Principal principal) throws IOException {
@@ -49,31 +57,52 @@ public class StorageService {
             throw new FileSizeLimitExceededException("Please reduce image" + image.getOriginalFilename(), image.getSize(), 5242880);
         }
 
-        Cloudinary cloudinary = new Cloudinary((ObjectUtils.asMap(
-                "cloud_name", cloud,
-                "api_key", key,
-                "api_secret", secret)));
+        Cloudinary cloudinary = getInstance();
 
         Map response = cloudinary.uploader().upload(image.getBytes(),
                 ObjectUtils.asMap("public_id", RandomStringUtils.randomAlphabetic(10)));
 
 
-        String imageUrl = (type.equals("IMAGE"))
-                ? cloudinary.url()
-                .transformation(new Transformation()
-                        .crop("fill")
-                        .width(300)
-                        .height(300))
-                .generate(response.get("public_id").toString())
-                : response.get("url").toString();
+        ImageDto imageDTO = new ImageDto();
+        if (type.equals("IMAGE")) {
+            imageDTO.setId(String.valueOf(ThreadLocalRandom.current().nextInt(100))).setUrl(
+                    cloudinary.url()
+                            .transformation(new Transformation()
+                                    .crop("fill")
+                                    .width(300)
+                                    .height(300))
+                            .generate(response.get("public_id").toString())
+            );
 
-
-        ImageDto imageDTO = new ImageDto()
-                .setId(String.valueOf(ThreadLocalRandom.current().nextInt(100))).setUrl(imageUrl);
+        } else {
+            String url = response.get("url").toString();
+            PostFile postFile = fileRepository.save(new PostFile().setUrl(url));
+            imageDTO.setId(String.valueOf(postFile.getId())).setUrl(url);
+        }
 
         DataResponse<ImageDto> dataResponse = new DataResponse<>();
         dataResponse.setTimestamp(Instant.now());
         dataResponse.setData(imageDTO);
         return dataResponse;
+    }
+
+    public Cloudinary getInstance() {
+        return new Cloudinary((ObjectUtils.asMap(
+                "cloud_name", cloud,
+                "api_key", key,
+                "api_secret", secret)));
+    }
+
+    public AccountResponse deleteImage(int id, Principal principal) throws IOException {
+        PostFile file = fileRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Нет такого файла"));
+        Cloudinary cloudinary = getInstance();
+        cloudinary.uploader().destroy(file.getUrl(), ObjectUtils.emptyMap());
+        fileRepository.delete(file);
+        AccountResponse accountResponse = new AccountResponse();
+        accountResponse.setTimestamp(ZonedDateTime.now().toInstant());
+        Map<String, String> dateMap = new HashMap<>();
+        dateMap.put("message", "file successfully deleted");
+        accountResponse.setData(dateMap);
+        return accountResponse;
     }
 }
