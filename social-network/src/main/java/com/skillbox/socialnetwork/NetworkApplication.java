92f65ef78140641be459cf229bcb5bdba7172467
Кот Артем <artem70107@gmail.com>
2021-11-08 22:00:49 +0300

    по мелочи исправил, но еще есть косяк

--- social-network/src/main/java/com/skillbox/socialnetwork/NetworkApplication.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/NetworkApplication.java
@@ -1,6 +1,7 @@
 package com.skillbox.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
