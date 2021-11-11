4d421562b773426b571995d3adeda479e8942110
Shkiller <nicklyashko9@gmail.com>
2021-11-11 22:43:59 +0300

    Notification message fix

--- social-network/src/main/java/com/skillbox/socialnetwork/controller/NotificationController.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/controller/NotificationController.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +27,7 @@ public class NotificationController {
         this.notificationService = notificationService;
     }
 
-    @PutMapping("/api/v1/notifications")
+    @GetMapping("/notifications")
     @PreAuthorize("hasAuthority('user:write')")
     public ResponseEntity<ListResponse<NotificationData>> notifications(@RequestParam(name = "offset", defaultValue = "0") int offset,
                                                                         @RequestParam(name = "itemPerPage", defaultValue = "20") int itemPerPage,
