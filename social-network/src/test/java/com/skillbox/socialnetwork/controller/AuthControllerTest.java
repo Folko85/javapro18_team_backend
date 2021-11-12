6fea22e7129e3acfd510d88ffdbee2c276de25ec
Gutorov <Folko85@yandex.ru>
2021-11-12 15:41:34 +0300

    Первый тест аутентификации

---
+++ social-network/src/test/java/com/skillbox/socialnetwork/controller/AuthControllerTest.java
@@ -0,0 +1,68 @@
+package com.skillbox.socialnetwork.controller;
+
+import com.skillbox.socialnetwork.AbstractTest;
+import com.skillbox.socialnetwork.NetworkApplication;
+import com.skillbox.socialnetwork.api.request.LoginRequest;
+import com.skillbox.socialnetwork.entity.Person;
+import com.skillbox.socialnetwork.repository.PersonRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest(classes = {NetworkApplication.class})
+@AutoConfigureMockMvc
+public class AuthControllerTest extends AbstractTest {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    private final String EMAIL = "test@test.ru";
+
+    private final String PASSWORD = "password";
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
+
+    @BeforeEach
+    public void setup() {
+        super.setup();
+    }
+
+    @AfterEach
+    public void cleanup() {
+        personRepository.deleteAll();
+
+    }
+
+    @Test
+    @DisplayName("Успешный вход в аккаунт c логином и паролем")
+    void testGetTags() throws Exception {
+        LoginRequest request = new LoginRequest();
+        request.setEMail(EMAIL);
+        request.setPassword(PASSWORD);
+        Person person = new Person();
+        person.setEMail(EMAIL);
+        person.setPassword(passwordEncoder.encode(PASSWORD));
+        person.setApproved(true);
+        person.setDateAndTimeOfRegistration(LocalDateTime.now().minusDays(1));
+        personRepository.save(person);
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
+                        .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
