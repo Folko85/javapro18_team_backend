1ed73fbf70fab1a6851cd5864ac12a00b353a452
Gutorov <Folko85@yandex.ru>
2021-11-15 00:26:03 +0300

    Переделал получение городов и стран с учётом пагинации

--- social-network/src/main/java/com/skillbox/socialnetwork/api/response/platformdto/PlaceDto.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/api/response/platformdto/PlaceDto.java
@@ -3,13 +3,15 @@ package com.skillbox.socialnetwork.api.response.platformdto;
 import com.skillbox.socialnetwork.api.response.Dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors (chain = true)
 @Schema(description = "Список городов")
-public class CityDto implements Dto {
+public class PlaceDto implements Dto {
 
-    @Schema (description = "Идентификатор города")
+    @Schema (description = "Идентификатор города/страны")
     int id;
-    @Schema (description = "Название города города")
+    @Schema (description = "Название города/страны")
     String title;
 }
