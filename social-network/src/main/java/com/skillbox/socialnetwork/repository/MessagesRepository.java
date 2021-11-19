669a1b0f49a73a40e682f8d7a68ed5390410b5a6
Gutorov <Folko85@yandex.ru>
2021-11-20 01:25:59 +0300

    Небольшой рефакторинг

--- social-network/src/main/java/com/skillbox/socialnetwork/repository/MessagesRepository.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/repository/MessagesRepository.java
@@ -3,26 +3,21 @@ package com.skillbox.socialnetwork.repository;
 import com.skillbox.socialnetwork.entity.SaveMessage;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import javax.annotation.PostConstruct;
 import java.util.Map;
 
-@Component
-public class RedisRepositoryImpl implements RedisRepository {
+@Repository
+public class MessagesRepository implements RedisRepository {
     private static final String KEY = "MESSAGES";
     private final RedisTemplate<String, Object> redisTemplate;
     private HashOperations<String, Object, Object> hashOperations;
 
-    public RedisRepositoryImpl(RedisTemplate<String, Object> redisTemplate) {
+    public MessagesRepository(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
-    /**
-     * В этом методе мы используем функцию с именем opsForHash(),
-     * она возвращает операции выполняемые с хеш-значениями,
-     * привязанными к данному ключу.
-     */
     @PostConstruct
     private void init() {
         hashOperations = redisTemplate.opsForHash();
--- social-network/src/main/java/com/skillbox/socialnetwork/repository/RedisRepository.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/repository/RedisRepository.java
@@ -1,12 +1,12 @@
 package com.skillbox.socialnetwork.repository;
 
 import com.skillbox.socialnetwork.entity.SaveMessage;
-import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 
-@Repository
+
 public interface RedisRepository {
+
     Map<Object, Object> findAllMessages();
 
     void add(SaveMessage movie);
