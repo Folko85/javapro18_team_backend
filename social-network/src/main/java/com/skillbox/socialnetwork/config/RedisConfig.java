8b0a1a8c2945fe3e403c09e2d7ec6401f5a52d08
Gutorov <Folko85@yandex.ru>
2021-11-19 23:30:49 +0300

    Привёл все зависимости к указанным в parent-проекте. Почистил RedisConfig

--- social-network/src/main/java/com/skillbox/socialnetwork/config/RedisConfig.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/config/RedisConfig.java
@@ -1,36 +1,26 @@
 package com.skillbox.socialnetwork.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-//    @Value("${spring.redis.host}")
-    private final String HOST = "localhost";
-//    @Value("${spring.redis.port}")
-    private final int PORT = 5000;
-//    @Value("${spring.redis.password}")
-    private final String PASSWORD = "r-edis1_pass4!";
-
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConFactory = new JedisConnectionFactory();
-        jedisConFactory.setHostName(HOST);
-        jedisConFactory.setPort(PORT);
-        jedisConFactory.setPassword(PASSWORD);
-        return jedisConFactory;
+        return new JedisConnectionFactory();
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 }
