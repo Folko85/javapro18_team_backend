b69d120c248c2178f30a76fc524271c58bc536c4
Gutorov <Folko85@yandex.ru>
2021-11-21 21:00:33 +0300

    Удалил ещё одно дублирующее DTO

--- social-network/src/test/java/com/skillbox/socialnetwork/controller/PostControllerTest.java
+++ social-network/src/test/java/com/skillbox/socialnetwork/controller/PostControllerTest.java
@@ -2,10 +2,13 @@ package com.skillbox.socialnetwork.controller;
 
 import com.skillbox.socialnetwork.AbstractTest;
 import com.skillbox.socialnetwork.NetworkApplication;
+import com.skillbox.socialnetwork.api.request.PostRequest;
 import com.skillbox.socialnetwork.entity.Person;
 import com.skillbox.socialnetwork.entity.Post;
+import com.skillbox.socialnetwork.entity.Tag;
 import com.skillbox.socialnetwork.repository.PersonRepository;
 import com.skillbox.socialnetwork.repository.PostRepository;
+import com.skillbox.socialnetwork.repository.TagRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,8 +20,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -31,7 +36,10 @@ public class PostControllerTest extends AbstractTest {
     private PostRepository postRepository;
 
     @Autowired
-    private PersonRepository accountRepository;
+    private TagRepository tagRepository;
+
+    @Autowired
+    private PersonRepository personRepository;
 
     Person person;
 
@@ -43,25 +51,59 @@ public class PostControllerTest extends AbstractTest {
         person.setPassword("password");
         person.setDateAndTimeOfRegistration(LocalDateTime.now().minusDays(5));
         person.setLastOnlineTime(LocalDateTime.now().minusDays(3));
-        accountRepository.save(person);
+        personRepository.save(person);
     }
 
     @AfterEach
     public void cleanup() {
-        accountRepository.deleteAll();
+        personRepository.deleteAll();
         postRepository.deleteAll();
     }
 
     @Test
     @WithMockUser(username = "test@test.ru", authorities = "user:write")
     void testGetPosts() throws Exception {
+        Post post = new Post();
+        post.setTitle("Title");
+        post.setPostText("Some Text. hi bro");
+        post.setDatetime(Instant.now());
+        post.setPerson(person);
+        postRepository.save(post);
         mockMvc.perform(MockMvcRequestBuilders
-                        .get("/api/v1/post")
+                        .get("/api/v1/feeds")
                         .principal(() -> "test@test.ru")
-                        .param("testText", "hi bro")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total").value(1));
+    }
+
+    @Test
+    @WithMockUser(username = "test@test.ru", authorities = "user:write")
+    void testPostWall() throws Exception {
+        PostRequest request = new PostRequest();
+        request.setTitle("Title");
+        request.setPostText("some text");
+        Tag tag = new Tag().setTag("tag");
+        tagRepository.save(tag);
+        request.setTags(List.of("tag"));
+        int id = personRepository.findByEMail("test@test.ru").get().getId();
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/users/{id}/wall", id)
+                        .principal(() -> "test@test.ru")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(request))
                         .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/feeds")
+                        .principal(() -> "test@test.ru")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.total").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data[0].tags[0]").value("tag"));
     }
 
     @Test
