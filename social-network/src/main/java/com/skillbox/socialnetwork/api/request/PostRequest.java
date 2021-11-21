cb89f0b4ed789c18da6415616265d91feac3b13d
Gutorov <Folko85@yandex.ru>
2021-11-21 19:17:48 +0300

    Добавляем теги в посты

--- social-network/src/main/java/com/skillbox/socialnetwork/api/request/PostRequest.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/api/request/PostRequest.java
@@ -3,9 +3,12 @@ package com.skillbox.socialnetwork.api.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PostRequest {
     private String title;
     @JsonProperty("post_text")
     private String postText;
+    private List<String> tags;
 }
