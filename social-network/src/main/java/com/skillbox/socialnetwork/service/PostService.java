8193b9c3adb8503663f13fa49f6df030ee216b0a
Gutorov <Folko85@yandex.ru>
2021-11-22 21:33:54 +0300

    Небольшая правка условий для варианта передачи несуществующего тега

--- social-network/src/main/java/com/skillbox/socialnetwork/service/PostService.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/service/PostService.java
@@ -64,16 +64,14 @@ public class PostService {
         Instant datetimeTo = (dateTo == -1) ? Instant.now() : Instant.ofEpochMilli(dateTo);
         Instant datetimeFrom = (dateFrom == -1) ? ZonedDateTime.now().minusYears(1).toInstant() : Instant.ofEpochMilli(dateFrom);
         List<Integer> blockers = friendshipService.getBlockersId(person.getId());
-
-
-        List<Integer> tags = Arrays.stream(tag.split("\\|")).filter(t -> !Objects.equals(t, ""))
-                .map(t -> tagRepository.findByTag(t).orElse(null))
-                .filter(Objects::nonNull).map(Tag::getId).collect(Collectors.toList());
         Page<Post> pageablePostList;
-        if (tags.isEmpty()) {
+        if (tag.equals("")) {
             pageablePostList = postRepository.findPostsByTextContainingByDateExcludingBlockersWithoutTags(text, author,
                     datetimeFrom, datetimeTo, pageable, blockers);
         } else {
+            List<Integer> tags = Arrays.stream(tag.split("\\|"))
+                    .map(t -> tagRepository.findByTag(t).orElse(null))
+                    .filter(Objects::nonNull).map(Tag::getId).collect(Collectors.toList());
             pageablePostList = postRepository.findPostsByTextContainingByDateExcludingBlockers(text, author, datetimeFrom,
                     datetimeTo, pageable, blockers, tags, tags.size());
         }
