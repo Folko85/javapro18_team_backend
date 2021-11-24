065367dbda17900dbb4788add64646c3f64cfdd4
Gutorov <Folko85@yandex.ru>
2021-11-25 01:44:18 +0300

    Кэш пишется. Добавил в дто instant-сериализатор/десериализатор, влиять вроде не должен

--- social-network/src/main/java/com/skillbox/socialnetwork/api/response/ListResponse.java
+++ social-network/src/main/java/com/skillbox/socialnetwork/api/response/ListResponse.java
@@ -2,6 +2,10 @@ package com.skillbox.socialnetwork.api.response;
 
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer;
+import com.skillbox.socialnetwork.api.DefaultInstantDeserializer;
 import lombok.Data;
 
 import java.time.Instant;
@@ -11,6 +15,8 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ListResponse<T extends Dto> {
     private String error;
+    @JsonSerialize(using = InstantSerializer.class)
+    @JsonDeserialize(using = DefaultInstantDeserializer.class)
     private Instant timestamp;
     private int total;
     private int offset;
