02631e2b1c24b69fdb1ee190b480ddfd90d1d4d7
Gutorov <Folko85@yandex.ru>
2021-12-07 19:14:54 +0300

    Вот это хорошо бы протестировать с помощью ngrok

--- zero-cloud-config/src/main/java/Team/zerocloudconfigserver/ZeroCloudConfigServerApplication.java
+++ zero-cloud-config/src/main/java/Team/zerocloudconfigserver/ZeroCloudConfigServerApplication.java
@@ -2,14 +2,15 @@ package Team.zerocloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RabbitAutoConfiguration.class})
 @EnableConfigServer
 public class ZeroCloudConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZeroCloudConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroCloudConfigServerApplication.class, args);
+    }
 
 }
