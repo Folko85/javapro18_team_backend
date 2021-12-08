109ac1ab356e08f484a0126d4b0f0c3398eb3dc4
Gutorov <Folko85@yandex.ru>
2021-12-08 15:54:16 +0300

    Добавил аннотаций

--- zero-cloud-config/src/main/java/Team/zerocloudconfigserver/ZeroCloudConfigServerApplication.java
+++ zero-cloud-config/src/main/java/Team/zerocloudconfigserver/ZeroCloudConfigServerApplication.java
@@ -2,12 +2,10 @@ package Team.zerocloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
 public class ZeroCloudConfigServerApplication {
